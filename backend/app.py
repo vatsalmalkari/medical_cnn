a7e2013c14433e2b722bd2984a6bb50425480181
Vatsal Malkari <66182569+vatsalmalkari@users.noreply.github.com>
2025-08-08 14:40:18 -0400

    Update app.py

--- backend/app.py
+++ backend/app.py
@@ -65,5 +65,6 @@ def predict():
 if __name__ == "__main__":
     import os
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='0.0.0.0', port=port)
+    app.run(host="0.0.0.0", port=port)
+
 
